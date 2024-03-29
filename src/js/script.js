4bad3c05cfaba1c721534855353b6ee9134fe1e4
yuhi1315 <yuhi1315@gmail.com>
2024-03-29 22:14:23 +0900

    スクロールアニメーションの付与

--- src/js/script.js
+++ src/js/script.js
@@ -51,4 +51,60 @@ jQuery(function ($) {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  //要素の取得とスピードの設定
+  var box = $(".information__img"),
+    speed = 700;
+
+  box.each(function () {
+    $(this).append('<div class="color"></div>');
+    var color = $(this).find($(".color")),
+      image = $(this).find("img");
+    var counter = 0;
+
+    image.css("opacity", "0");
+    color.css("width", "0%");
+    color.on("inview", function () {
+      console.log("test");
+      if (counter == 0) {
+        $(this)
+          .delay(200)
+          .animate({ width: "100%" }, speed, function () {
+            image.css("opacity", "1");
+            $(this).css({ left: "0", right: "auto" });
+            $(this).animate({ width: "0%" }, speed);
+          });
+        counter = 1;
+      }
+    });
+  });
+
+  //要素の取得とスピードの設定
+  var box = $(".information__img"),
+    speed = 700;
+
+  //.colorboxの付いた全ての要素に対して下記の処理を行う
+  box.each(function () {
+    $(this).append('<div class="color"></div>');
+    var color = $(this).find($(".color")),
+      image = $(this).find("img");
+    var counter = 0;
+
+    image.css("opacity", "0");
+    color.css("width", "0%");
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on("inview", function () {
+      console.log("test");
+      if (counter == 0) {
+        $(this)
+          .delay(200)
+          .animate({ width: "100%" }, speed, function () {
+            image.css("opacity", "1");
+            $(this).css({ left: "0", right: "auto" });
+            $(this).animate({ width: "0%" }, speed);
+          });
+        counter = 1;
+      }
+    });
+  });
 });
