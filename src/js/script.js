4b13b464d6c83f197cae9ab3ccbaad392e2c88d4
yuhi1315 <yuhi1315@gmail.com>
2024-03-29 10:28:36 +0900

    スワイパーを実装

--- src/js/script.js
+++ src/js/script.js
@@ -16,12 +16,12 @@ jQuery(function ($) {
     $(".js-sp-nav").fadeOut();
     $(".js-hamburger").removeClass("is-active");
   }
-  const swiper = new Swiper(".swiper", {
+  const mv__swiper = new Swiper(".mv__swiper", {
     loop: true,
-    // autoplay: {
-    //   //追記
-    //   delay: 0, //追記
-    // },
+    autoplay: {
+      //追記
+      delay: 0, //追記
+    },
     speed: 3000,
     pagination: {
       el: ".swiper-pagination",
@@ -30,6 +30,39 @@ jQuery(function ($) {
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
+    },
+  });
+  const campaign__swiper = new Swiper(".campaign__swiper", {
+    // loop: true,
+    // loop: true,
+    spaceBetween: 24,
+    slidesPerView: "1.5",
+    speed: 2000,
+    // loopAdditionalSlides: 2,
+    // width: 280,
+
+    // autoplay: {
+    //   delay: 0,
+    //   disableOnInteraction: false,
+    // },
+    breakpoints: {
+      //ブレークポイントの設定 小さい順に設定する！！
+      768: {
+        slidesPerView: 3.5,
+        spaceBetween: 40,
+        // width: 1265.5,
+        // },
+        // 1920: {
+        //   slidesPerView: "5",
+        //   spaceBetween: 40,
+        //   width: 1825,
+        // },
+      },
+      // 前後の矢印
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
     },
   });
 });
