7489069057701ea14b330fbd01155b360648d001
yuhi1315 <yuhi1315@gmail.com>
2024-04-11 14:27:01 +0900

    事前修正：テキストインデントで調整

--- src/js/script.js
+++ src/js/script.js
@@ -104,32 +104,6 @@ jQuery(function ($) {
   });
 
   //要素の取得とスピードの設定
-  var box = $(".information__img"),
-    speed = 700;
-
-  box.each(function () {
-    $(this).append('<div class="color"></div>');
-    var color = $(this).find($(".color")),
-      image = $(this).find("img");
-    var counter = 0;
-
-    image.css("opacity", "0");
-    color.css("width", "0%");
-    color.on("inview", function () {
-      if (counter == 0) {
-        $(this)
-          .delay(200)
-          .animate({ width: "100%" }, speed, function () {
-            image.css("opacity", "1");
-            $(this).css({ left: "0", right: "auto" });
-            $(this).animate({ width: "0%" }, speed);
-          });
-        counter = 1;
-      }
-    });
-  });
-
-  //要素の取得とスピードの設定
   var box = $(".js-imgBg-change"),
     speed = 700;
 
